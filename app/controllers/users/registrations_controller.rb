27172c1057740ed3fcdf264530abc70794bfbf2b
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-26 14:34:30 +0900

    [Add] : #11  registrations_controllerにアクションを追加 ゲストユーザーの削除、メール・パスワードを変更できないようregistrations、password_comtrollerににアクションを追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :ensure_normal_user, only: %i[update destroy]
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
+    end
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -59,4 +66,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+
 end
