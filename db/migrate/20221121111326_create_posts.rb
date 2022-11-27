41d0c4ac129839bbd396dfe7c5a1f0cc644fbe07
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-27 17:33:30 +0900

    [Update] : #16 登録アクションの実装

--- db/migrate/20221121111326_create_posts.rb
+++ db/migrate/20221121111326_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :name
+      t.string :desk_name
       t.text :description
+      t.integer :user_id
 
       t.timestamps
     end
