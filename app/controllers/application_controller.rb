5f76d1adf579508ce581abe2fd0c7334be471c3b
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-28 22:13:47 +0900

    [Update] : #16 Signup時にusernameがDBに登録されていなかったため修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # サインアップ時にusernameも追加で許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    #プロフィール編集時にnameと自己紹介、アイコン画像のストロングパラメーターを渡す設定
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :introduce, :user_image])
+  end
 end
