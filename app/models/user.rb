5ee902f19fc45e0df6e17a71cc83f22fabeff243
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-12-05 10:04:25 +0900

    [Add] : #26 未ログイン時, ログイン時の表示を修正 未ログイン時にゲストログイン, ログイン, 新規登録btnを表示 ログイン時にマイページ, ログアウトbtn, user_imageを表示 user_imageの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
+  has_one_attached :user_image
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+      user.username = "ゲストユーザー"
     end
   end
   devise :database_authenticatable, :registerable,
