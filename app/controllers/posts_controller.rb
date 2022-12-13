624dd098515840e1e527f16cd9e17c903caad86c
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-12-13 18:07:49 +0900

    [Add] : #28 投稿時にカテゴリーを選択できるようcheck_box追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:id, :description, :desk_image)
+      params.require(:post).permit(:id, :description, :desk_image, category_ids:[])
     end
 end
