ce58a4a69b70b9e2c8d8a0c2dd7e3acfb330fae1
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-01-11 15:56:35 +0900

    [Add] #38 Topページにbg-img, いいね数ランキングを追加
    いいね数ランキングは3つまで表示するようにcontroller追記。
    いいね数、コメント数が表示されるよう追記。

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,6 @@
 class TopsController < ApplicationController
+  MAX_LIKES_COUNT = 3
   def index
-  end
-
-  def create
+    @posts = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(MAX_LIKES_COUNT).pluck(:post_id))
   end
 end
