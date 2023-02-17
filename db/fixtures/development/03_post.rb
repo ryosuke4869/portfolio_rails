8a730adb8e0f50200fc479747cc09b8ab27f1562
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-02-17 11:47:42 +0900

    [Add] #48 初期データに新たなUser, avatar_imageを複数追加 desk_imageを複数追加

--- db/fixtures/development/03_post.rb
+++ db/fixtures/development/03_post.rb
@@ -1,8 +1,27 @@
-5.times do |n|
-  Post.create!(
+3.times do |n|
+  post = Post.create!(
     id: n,
     description: "テスト#{n + 1}",
-    desk_images: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('public/images/322580172.jpeg')), filename: '322580172.jpeg'),
+    desk_images: ActiveStorage::Blob.create_and_upload!(
+      io: File.open(Rails.root.join('public/images/post_images/Post_image1.jpeg')),
+      filename: 'Post_image1.jpeg'),
     user_id: User.offset(rand(User.count)).first.id
   )
+  post.desk_images.attach(io: File.open(Rails.root.join('public/images/post_images/Post_image6.jpeg')),
+                  filename: 'Post_image6.jpeg')
 end
+
+3.times do |n|
+  post = Post.create!(
+    id: n+3,
+    description: "テスト#{n + 4}",
+    desk_images: ActiveStorage::Blob.create_and_upload!(
+      io: File.open(Rails.root.join('public/images/post_images/Post_image2.jpeg')),
+      filename: 'Post_image2.jpeg'),
+    user_id: User.offset(rand(User.count)).first.id
+  )
+  post.desk_images.attach(io: File.open(Rails.root.join('public/images/post_images/Post_image3.jpeg')),
+                  filename: 'Post_image3.jpeg')
+end
+
+
