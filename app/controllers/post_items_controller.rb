c49fb9077c670407444f59a0112ee5bd5f4bfa12
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-03-02 18:51:04 +0900

    [Add] #52 登録できるitem数の上限を設定, 検索欄のレイアウトを修正

--- app/controllers/post_items_controller.rb
+++ app/controllers/post_items_controller.rb
@@ -1,11 +1,23 @@
 class PostItemsController < ApplicationController
   before_action :set_post
+
   def create
     @item = Item.find_by(name: params[:item_name])
     @post_item = PostItem.new(post_id: @post.id, item_id: @item.id)
-    if @post_item.save
-      redirect_to post_path(@post.id)
-      flash[:notice] = "アイテムを登録しました。"
+
+    item_count = PostItem.where(post_id: @post.id).count
+    binding.pry
+
+    if @post_item.valid?
+      if item_count < 12
+        binding.pry
+        @post_item.save
+        redirect_to post_path(@post.id)
+        flash[:notice] = "アイテムを登録しました。"
+      else
+        redirect_to post_path(@post.id)
+        flash[:notice] = "アイテムは12個までしか登録できません。"
+      end
     else
       flash[:alert] = "アイテムの登録に失敗しました."
     end
