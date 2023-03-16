cd4f600f987e13300f9154203d0f28d9d09f90d1
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-03-16 14:25:47 +0900

    [Add] #58 swipperのレイアウトを変更

--- app/javascript/packs/swipper.js
+++ app/javascript/packs/swipper.js
@@ -2,8 +2,19 @@ $(document).on('turbolinks:load',function(){
   const swiper = new Swiper(".swiper", {
     // ページネーションが必要なら追加
     pagination: {
-      el: ".swiper-pagination"
+      el: ".swiper-pagination",
     },
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "2",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true
+      },
     // ナビボタンが必要なら追加
     navigation: {
       nextEl: ".swiper-button-next",
