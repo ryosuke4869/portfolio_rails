e05bbaf88f6c83893b2fcaac937b221b5249c957
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-04-24 20:20:59 +0900

    [Add] #67 systemspecでjs:trueを使用できていなかったためコード変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,44 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 
+# Capybaraテストフレームワークを使用た自動テスト用の新しいドライバーを登録 (system specを使用するためのコード）
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: options.arguments,
+    }
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    capabilities: capabilities,
+  )
+end
+
 RSpec.configure do |config|
+  config.before(:each, type: :system, js: true) do #JavaScript を有効
+    if ENV["SELENIUM_DRIVER_URL"].present? #環境変数が存在するかどうかを確認
+      #環境変数があった場合のドライバー構成
+      driven_by :selenium, using: :chrome, options: {
+        browser: :remote,
+        url: ENV.fetch("SELENIUM_DRIVER_URL"),
+
+      }
+      Capybara.server_host = 'web'
+      Capybara.app_host="http://#{Capybara.server_host}"
+    else
+      #ない場合の
+      driven_by :selenium_chrome_headless
+    end
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   # Factory_botの設定
@@ -72,27 +109,4 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
-
-  config.before(:each, type: :system, js: true) do
-    if ENV["SELENIUM_DRIVER_URL"].present?
-      driven_by :selenium, using: :chrome, options: {
-        browser: :remote,
-        url: ENV.fetch("SELENIUM_DRIVER_URL"),
-        desired_capabilities: :chrome,
-      }
-    else
-      driven_by :selenium_chrome_headless
-    end
-  end
-end
-
-# Capybaraテストフレームワークを使用た自動テスト用の新しいドライバーを登録
-Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
