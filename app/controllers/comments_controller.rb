54158ae492d92324a4346bf3f24675dba48453f1
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-05-15 07:33:19 +0900

    [test] #67 comment機能のテストコード追加（systemspec）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,23 +5,12 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to request.referer, notice: "コメントを投稿しました"
     else
-      flash[:danger] = "投稿に失敗しました"
+      flash[:danger] = "コメントの投稿に失敗しました"
       redirect_back(fallback_location: root_path)
-    end
-
-    def edit
-      @post = Post.find(params[:post_id])
-      @comment = Comment.find(params[:id])
-      if @comment.update(comment_params)
-        redirect_to post_path(@post), notice: "コメントを編集しました"
-      else
-        flash.now[:danger] = "編集に失敗しました"
-        render 'edit'
-      end
     end
   end
 
-  def desstroy
+  def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
     flash[:danger] = "コメントを削除しました"
--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -2,45 +2,96 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :system do
   describe 'CommentのCRUD' do
+    let(:user) { create(:user) }
+    let(:user_other) { create(:user) }
+    let!(:post) { create(:post, user: user) }
+    let!(:post_other) { create(:post, user: user_other) }
+    let!(:post_category_engineer) { create(:post, :category_engineer) }
+    let!(:post_category_writer) { create(:post, :category_writer) }
+    let!(:post_category_gamer) { create(:post, :category_gamer) }
+    let!(:comment) { create(:comment, post: post, user: user) }
+    let!(:comment_others) { create(:comment, post: post_other, user: user) }
+
+    before do
+      visit post_path(post.id)
+    end
+
     describe 'コメントの一覧' do
       context 'コメントされている場合' do
         it 'コメントが表示される' do
-        # userの名前が表示される
-        # userのavatar画像が表示される
-        # コメントした日時が表示される
-        # コメントの本文が表示される
+          # userの名前が表示される
+          expect(page).to have_content comment.user.name
+          # userのavatar画像が表示される
+          expect(page).to have_selector("img[src$='sample_icon.jpeg']")
+          # コメントの本文が表示される
+          expect(page).to have_content comment.body
         end
-      end
-      context 'コメントがされていない場合' do
-        it '' do
-
-        end
-
       end
     end
+
     describe 'コメントの作成' do
-      it 'コメントの作成に成功する' do
+      context 'ログインしている場合' do
+        before do
+          sign_in(user)
+          visit post_path(post.id)
+        end
+        it 'コメントの作成に成功する' do
+          # コメント記入欄にコメント本文を記入
+          fill_in 'コメント記入欄', with: 'コメントテスト文'
+          # コメントするbrnを押す
+          click_on 'コメントする'
+          # 投稿したコメントが表示される
+          expect(page).to have_content('コメントを投稿しました')
+        end
 
+        it 'コメントの作成に失敗する' do
+          # コメント記入欄にコメント本文を未記入
+          fill_in 'コメント記入欄', with: ' '
+          # コメントするbtnを押す
+          click_on 'コメントする'
+          # コメント失敗の表示される
+          expect(page).to have_content('コメントの投稿に失敗しました')
+        end
       end
 
-      it 'コメントの作成に失敗する' do
-
+      context 'ログインしていない場合' do
+        it 'コメント記入欄とコメントするbtnが表示されtない' do
+          # コメント記入欄が表示されていない
+          expect(page).not_to have_content('コメント記入欄')
+          # コメントするbtnが表示されない
+          expect(page).not_to have_content('コメントする')
+        end
       end
-
     end
 
     describe 'コメントの削除' do
       context '他人のコメントの場合' do
         it '削除ボタンが表示されない' do
-
+          expect(page).not_to have_link '削除'
         end
       end
-      context '自身のコメントの場合' do
-        it '削除を行うことができる'
+      context '自身のコメント, 自身の投稿のコメントの場合' do
+        before do
+          sign_in(user)
+        end
+
+        it '自身のコメントの削除を行うことができる' do
+          visit post_path(post_other.id)
+          within '.comment-delete' do
+            click_on '削除'
+          end
+          expect(page).to have_content('コメントを削除しました')
+        end
+
+        it '他人のコメントの削除を行うことができる' do
+          visit post_path(post.id)
+          within '.comment-delete' do
+            click_on '削除'
+          end
+          expect(page).to have_content('コメントを削除しました')
         end
       end
     end
-
   end
 end
 
