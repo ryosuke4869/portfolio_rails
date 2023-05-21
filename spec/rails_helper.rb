c47d0149a0d6692b0810ee5db8ea24ecf43e6ba6
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-05-21 15:29:54 +0900

    [change] #67 selenium-webdriverのバージョン変更に伴いコードを変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,41 +32,34 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 # Capybaraテストフレームワークを使用た自動テスト用の新しいドライバーを登録 (system specを使用するためのコード）
-Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
+Capybara.register_driver :remote_chrome do |app|
+  url = "http://chrome:4444/wd/hub"
+  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions" => {
+      "args" => [
+        "no-sandbox",
+        "headless",
+        "disable-gpu",
+        # ディスクのメモリスペースを使用。
+        'disable-dev-shm-usage',
+        'remote-debugging-port=9222'
 
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: options.arguments,
+      ]
     }
   )
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    capabilities: capabilities,
-  )
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
 end
 
 RSpec.configure do |config|
-  config.before(:each, type: :system, js: true) do #JavaScript を有効
-    if ENV["SELENIUM_DRIVER_URL"].present? #環境変数が存在するかどうかを確認
-      #環境変数があった場合のドライバー構成
-      driven_by :selenium, using: :chrome, options: {
-        browser: :remote,
-        url: ENV.fetch("SELENIUM_DRIVER_URL"),
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
 
-      }
-      Capybara.server_host = 'web'
-      Capybara.app_host="http://#{Capybara.server_host}"
-    else
-      #ない場合の
-      driven_by :selenium_chrome_headless
-    end
+  config.before(:each, type: :system, js: true) do
+    driven_by :remote_chrome
+    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    Capybara.server_port = 3000
+    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
   end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -106,7 +99,4 @@ RSpec.configure do |config|
   config.extend ControllerMacros, :type => :controller
   config.include ActiveStorageValidations::Matchers
   config.include SignInSupport
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
 end
