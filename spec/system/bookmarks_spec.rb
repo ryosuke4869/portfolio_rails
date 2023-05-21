7083d2d158f27e1d4b0ec6c2592cc1edaacc7aed
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-05-21 16:20:28 +0900

    [test] #67 ブックマーク機能のテストコード追加(systemspec)

--- spec/system/bookmarks_spec.rb
+++ spec/system/bookmarks_spec.rb
@@ -1,34 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe "Bookmark", type: :system do
+RSpec.describe "Bookmark", type: :system, js: true  do
   describe 'BookmarkのCRUD' do
-    describe 'bookmarkの作成' do
-      context '他ユーザーの投稿, ログインしている場合' do
-        it 'bookmarkが作成できる' do
+    let(:user) { create(:user) }
+    let!(:post_user) { create(:post, user: user) }
+    let!(:post_others) { create(:post) }
+    let!(:post_others_bookmarked) { create(:post) }
+    let!(:bookmark) { create(:bookmark, post: post_others_bookmarked, user: user) }
+    let!(:post_category_engineer) { create(:post, :category_engineer) }
+    let!(:post_category_writer) { create(:post, :category_writer) }
+    let!(:post_category_gamer) { create(:post, :category_gamer) }
 
-        end
-      end
-
-      context '自分の投稿, ログインしていない場合' do
-        it '保存をclickできない' do
-
-        end
-      end
-
-    end
-
-    describe 'bookmarkの削除' do
+    describe 'ブックマークの作成と解除' do
       context 'ログインしている場合' do
-        it '保存を解除できる' do
-
+        before do
+          sign_in(user)
         end
-
+        fit 'ブックマークを作成できる' do
+          visit post_path(post_others)
+          click_on "btn-bookmark-#{post_others.id}"
+          expect(page).to have_selector('.fas.fa-bookmark')
+          expect(post_others.bookmarks.count).to eq(1)
+        end
+        it 'ブックマークを解除できる' do
+          visit post_path(post_others_bookmarked)
+          click_on "btn-unbookmark-#{post_others_bookmarked.id}"
+          expect(page).to have_selector('.far.fa-bookmark')
+          expect(post_others_bookmarked.bookmarks.count).to eq(0)
+        end
       end
+
+      context '自分の投稿の場合' do
+        it 'ブックマークをclickすることができない' do
+          sign_in(user)
+          visit post_path(post_user)
+          expect(page).to have_css('.mypost-bookmark')
+        end
+      end
+
       context 'ログインしていない場合' do
-        it '保存をclickできない' do
-
+        it 'いいねをclickすることができない' do
+          visit post_path(post_others)
+          expect(page).to have_css('.nologin-bookmark')
         end
       end
+
     end
   end
 end
