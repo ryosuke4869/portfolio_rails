54b711be0567d0475053ac03fd4b0ce0ae9abcec
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2023-07-05 23:42:50 +0900

    [style, change] #71 ransackactionのコードを変更, レイアウトの修正
    ransackが提供するフォームヘルパーsearch_form_forに変更。
    変更に伴いレイアウトを変更

--- app/controllers/concerns/ransack_search_actions.rb
+++ app/controllers/concerns/ransack_search_actions.rb
@@ -1,22 +1,23 @@
 module RansackSearchActions
   extend ActiveSupport::Concern
+
   def ransack_search
     @q = Post.ransack(params[:q])
     @posts = @q.result.includes(:categories).page(params[:page]).per(6)
-
-    if params[:category].present? && params[:category] != "指定なし"
-      @posts = @posts.joins(:categories).where(categories: { name: params[:category] })
-    end
-
-    if params[:sort_by] == "likes"
-      sort_order = params[:sort_order] == "asc" ? "ASC" : "DESC"
-      @posts = @posts.left_joins(:likes).group(:id).order(Arel.sql("COUNT(likes.id) #{sort_order}"))
-    else
-      sort_order = params[:sort_order].presence || "desc"
-      if sort_order == "指定なし"
-        @posts = @posts.order(created_at: nil)
+    if params[:q].present?
+      if params[:q][:category_eq].present? && params[:q][:category_eq] != ''
+        @posts = @posts.joins(:categories).where(categories: { name: params[:q][:category_eq] })
+      end
+      if params[:q][:s] == "likes"
+        sort_order = params[:q][:s_sort_order] == "asc" ? "ASC" : "DESC"
+        @posts = @posts.left_joins(:likes).group(:id).order(Arel.sql("COUNT(likes.id) #{sort_order}"))
       else
-        @posts = @posts.order(created_at: sort_order)
+        sort_order = params[:q][:s_sort_order].presence || "desc"
+        if sort_order == "指定なし"
+          @posts = @posts.order(created_at: nil)
+        else
+          @posts = @posts.order(created_at: sort_order)
+        end
       end
     end
   end
